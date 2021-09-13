6fdc8cf4c3ae0240ea7f34b3446c57a5038480c2
Dave Gershman <davegershman465@gmail.com>
2021-09-12 23:19:27 -0400

    commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,34 +3,24 @@
  *  Copyright 2021 Dave Gershman
  */
 
-import java.util.Hashtable;
-
 public class App
 {
-    public static double GetBAC(double A, double W, double r, double H) {
-        return (A * 5.14 / W * r) - 0.015 * H;
-    }
-
     public static void main( String[] args )
     {
-        int gender = Input.GetInt("Enter a 1 if you are male or a 2 if you are female: ");
-        double oz_alcohol = Input.GetDouble("How many ounces of alcohol did you have? ");
+        double height = Input.GetDouble("What is your height in inches? ");
         double weight = Input.GetDouble("What is your weight in pounds? ");
-        double hours = Input.GetDouble("How many hours has it been since your last drink? ");
 
-        double r = 0f;
-        if (gender == 1)
-            r = 0.73f;
-        else if (gender == 2)
-            r = 0.66f;
+        double bmi = (weight / (height * height)) * 703;
 
-        double BAC = GetBAC(oz_alcohol, weight, r, hours);
+        System.out.printf("Your BMI is %.1f.\n", bmi);
 
-        System.out.printf("Your BAC is %.6f\n", BAC);
+        if (bmi >= 18.5 && bmi <= 25)
+            System.out.print("You are within the ideal weight range.");
 
-        if (BAC >= 0.08)
-            System.out.print("It is not legal for you to drive.");
-        else
-            System.out.print("It is legal for you to drive.");
+        if (bmi > 25)
+            System.out.print("You are overweight. You should see your doctor.");
+
+        if (bmi < 18.5)
+            System.out.print("You are underweight. You should see your doctor.");
     }
 }
